b2424619c2e4183a7dd6bf197428547b2c2a5f53
Kenny Umobi <umobieke@msu.edu>
2024-04-21 03:22:38 -0400

    first commit, made a website using svelte API, it just reads each post in the posts array and displays its title and body.

---
+++ src/App.svelte
@@ -0,0 +1,6 @@
+<!-- src/App.svelte -->
+<script>
+	import DataDisplay from './components//DataDisplay.svelte';
+</script>
+
+<DataDisplay />
---
+++ src/components/DataDisplay.svelte
@@ -0,0 +1,27 @@
+<!-- src/DataDisplay.svelte -->
+<script>
+  import { onMount } from 'svelte';
+  
+  let posts = [];
+
+  async function fetchData() {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const json = await response.json();
+      posts = json;
+  }
+
+  onMount(fetchData);
+</script>
+
+{#if posts.length > 0}
+  <ul>
+      {#each posts as post}
+          <li>
+              <h3>{post.title}</h3>
+              <p>{post.body}</p>
+          </li>
+      {/each}
+  </ul>
+{:else}
+  <p>Loading...</p>
+{/if}
